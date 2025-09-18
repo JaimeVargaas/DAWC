655431255ba01f43276b61a821b044b65a195b0a
JaimeVargaas <jvarbae782@g.educaand.es>
2025-09-18 11:16:20 +0200

    first commit

---
+++ Pruebas/js/main.js
@@ -0,0 +1,6 @@
+// No es necesario crear la funcion main para que funcione, se puede poner a pelo y es lo mismo
+
+document.getElementById("h11").style.backgroundColor = "red";
+document.getElementById("h11").style.color = "white";
+
+/******************************FUNCTIONS******************************/
