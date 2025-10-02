739016b0c927727ea193a670b7b85049d5398122
Jaime Vargas <jvarbae782@g.educaand.es>
2025-10-02 11:24:01 +0200

    ejer4 prac1

--- Práctica 1/main.js
+++ Práctica 1/main.js
@@ -12,10 +12,39 @@ document.getElementById("añadir").onclick = function() {
     let alumnos = [nombre.value, apellidos.value, curso.value, notas];
 
     for(let i=0;i<alumnos.length-1;i++){
-        parrafoAlum.innerHTML += alumnos[i] + "-";
-        for(let j=0)
+        parrafoAlum.innerHTML += alumnos[i] + " - ";
     }
 
+     for(let i=0;i<alumnos.length-1;i++){
+        if(i==alumnos.length-2)
+            parrafoAlum.innerHTML += alumnos[3][i];
+        else
+            parrafoAlum.innerHTML += alumnos[3][i] + ", ";
+    }
+    parrafoAlum.innerHTML += "<br>"
+}
+
+// Ejercicio 2
 
 
+// Ejercicio 3
+document.getElementById("convert").onclick = function() {
+    let euros = document.getElementById("euros").value;
+    let resDol = document.getElementById("dolares");
+    let resYen = document.getElementById("yenes");
+
+    resDol.innerText = (euros*1.18);
+    resYen.innerText = (euros*173.0);
+}
+
+// Ejercicio 4
+let radio = document.getElementById("radio");
+let resultadoCalculo = document.getElementById("resCal");
+
+document.getElementById("calArea").onclick = function() {
+    resultadoCalculo.innerText= (2*3.14*Math.pow(radio.value,2));
+}
+
+document.getElementById("calPeri").onclick = function() {
+    resultadoCalculo.innerText=(2*3.14*radio.value);
 }
