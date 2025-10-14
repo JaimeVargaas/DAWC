29c507f161d96523eefb0f62ebba94d08d30ba09
Jaime Vargas <jvarbae782@g.educaand.es>
2025-10-14 13:00:03 +0200

    pruebas date y navigator

--- Pruebas/js/pruebaObjetos.js
+++ Pruebas/js/pruebaObjetos.js
@@ -1,94 +1,133 @@
+// let info = document.getElementById("info");
+
+// let obj1 = {
+//     nombre: "Pepe",
+//     edad: 30,
+//     email: "pepe@mail.com"
+// }
+
+// // writable sirve para poder cambiar su valor cuando queramos, y enumerable para que salga o no en la consola y en los valores del array de object.keys
+// Object.defineProperty(obj1, "color",{writable: true,enumerable: true, value:"red"});
+// obj1.color="yellow";
+
+// // para que no le pueda añadir propiedades a un objeto
+// Object.preventExtensions(obj1);
+
+// let obj2 = new Object ({
+//     nombre: "María",
+//     edad: 23,
+//     email: "maria@mail.com"
+// });
+
+// Object.defineProperties(obj2, {
+//     "color":{writable: false,enumerable: true   , value:"blue"},
+//     "notas":{writable: false,enumerable: true, value:[6,7,8]}
+// });
+// Object.defineProperty(obj2, "color de fondo",{writable: true,enumerable: true, value:"red"});
+
+// // para que no le pueda añadir propiedades a un objeto
+// Object.preventExtensions(obj2);
+
+// if(Object.isExtensible(obj2))
+//     obj2.colordefondo = "Green"
+// else
+//     console.log("no es extensible")
+
+// // no deja ni borrar ni añadir, pero puede cambiar el valor de las propiedades
+// Object.seal(obj2);
+// if(!Object.isSealed(obj2))
+//     obj2.nombre = "Green"
+// else
+//     console.log("no es admin")
+
+// info.innerHTML += "Datos de pepe: " + obj1.nombre + " " + obj1.edad + " " + obj1.email + " " + obj1.color +"<br>";
+// info.innerHTML += "Datos de María: " + Object.keys(obj2); 
+
+// console.log("Recorriendo ob1")
+// for (prop in obj1) 
+//     console.log(obj1[prop])
+
+// console.log("Recorriendo ob2")
+// for (prop of Object.keys(obj2))
+//     console.log("Propiedad " + prop + ". Value: " + obj2[prop])
+
+// // eval("document.open(); document.write('<h1>Web eliminada</h1>')")
+
+// let array2 = ["hola", 1, "adios", {nombre:"PEPE",edad:2, mail:"pepe@mail.es"}];
+// // para eliminiar el ultimo objeto de un array
+// array2.pop();
+
+// // para añadir algo en mitad del array
+// array2.splice(3,0,false,99999,"Pepe");
+// info.innerText="";
+// for(let i in array2)  {
+//     if (i==0)
+//         info.innerText += array2[i];
+//     else
+//         info.innerText += " - "+ array2[i]  ;
+// }
+
+// /*
+//  Números sean negativos
+//  true a false y viceversa
+//  Los string se le anteponga "Nombre: "
+//  */
+
+//  // la funcion map lleva dentro un bucle por lo tanto no hace falta crearlo dentro
+// let myArrayResult = array2.map(function (elem) {
+//     switch(typeof elem) {
+//         case "number":
+//             return (elem * -1);
+//         case "boolean":
+//             return !elem;
+//         case "string":
+//             return "Nombre: " + elem;
+//     }
+// });
+
+// info.innerHTML = myArrayResult;
+
+// // esto sirve para iterar el array
+// info.innerHTML = "";
+// myArrayResult.forEach(function(elem,indice,arrayActual){
+//     console.log("myArray es: " + arrayActual);
+//     info.innerHTML += "<br> Elemento actual es: " + elem + " situado en la posición: " + indice;
+// });
+
+// let existeBoolean = false;
+
+// existeBoolean = myArrayResult.some(elem => {
+//     return typeof elem == "boolean";
+// });
+
+
+// // fechas
+// let dateNow = new Date(1980,6,31,23,54,15);
+
+
+// script que si son mas de las 8 de la tarde, que ponga en colores oscuros la web
 let info = document.getElementById("info");
+let main = document.getElementById("main");
+let footer = document.getElementById("footer");
+let aside = document.getElementById("aside");
 
-let obj1 = {
-    nombre: "Pepe",
-    edad: 30,
-    email: "pepe@mail.com"
+
+let dateNow = new Date();
+console.log(dateNow.getMonth());
+
+info.innerText = dateNow.toLocaleString();
+if(dateNow.getHours()>=20 || dateNow.getHours()<=7 && dateNow.getMonth()>=9 || dateNow.getMonth()<=2) {
+    info.style.color="White";
+    main.style.backgroundColor = "grey";
+    footer.style.backgroundColor = "black";
+    aside.style.backgroundColor = "darkblue";
 }
 
-// writable sirve para poder cambiar su valor cuando queramos, y enumerable para que salga o no en la consola y en los valores del array de object.keys
-Object.defineProperty(obj1, "color",{writable: true,enumerable: true, value:"red"});
-obj1.color="yellow";
+info.innerHTML = "¿Estás conectado a Internet?: " + navigator.userAgent; 
 
-// para que no le pueda añadir propiedades a un objeto
-Object.preventExtensions(obj1);
 
-let obj2 = new Object ({
-    nombre: "María",
-    edad: 23,
-    email: "maria@mail.com"
-});
 
-Object.defineProperties(obj2, {
-    "color":{writable: false,enumerable: true   , value:"blue"},
-    "notas":{writable: false,enumerable: true, value:[6,7,8]}
-});
-Object.defineProperty(obj2, "color de fondo",{writable: true,enumerable: true, value:"red"});
 
-// para que no le pueda añadir propiedades a un objeto
-Object.preventExtensions(obj2);
-
-if(Object.isExtensible(obj2))
-    obj2.colordefondo = "Green"
-else
-    console.log("no es extensible")
-
-// no deja ni borrar ni añadir, pero puede cambiar el valor de las propiedades
-Object.seal(obj2);
-if(!Object.isSealed(obj2))
-    obj2.nombre = "Green"
-else
-    console.log("no es admin")
-
-info.innerHTML += "Datos de pepe: " + obj1.nombre + " " + obj1.edad + " " + obj1.email + " " + obj1.color +"<br>";
-info.innerHTML += "Datos de María: " + Object.keys(obj2); 
-
-console.log("Recorriendo ob1")
-for (prop in obj1) 
-    console.log(obj1[prop])
-
-console.log("Recorriendo ob2")
-for (prop of Object.keys(obj2))
-    console.log("Propiedad " + prop + ". Value: " + obj2[prop])
-
-// eval("document.open(); document.write('<h1>Web eliminada</h1>')")
-
-let array2 = ["hola", 1, "adios", {nombre:"PEPE",edad:2, mail:"pepe@mail.es"}];
-// para eliminiar el ultimo objeto de un array
-array2.pop();
-
-// para añadir algo en mitad del array
-array2.splice(3,0,false,99999,"Pepe");
-info.innerText="";
-for(let i in array2)  {
-    if (i==0)
-        info.innerText += array2[i];
-    else
-        info.innerText += " - "+ array2[i]  ;
-}
-
-/*
- Números sean negativos
- true a false y viceversa
- Los string se le anteponga "Nombre: "
- */
-
- // la funcion map lleva dentro un bucle por lo tanto no hace falta crearlo dentro
-let myArrayResult = array2.map(function (elem) {
-    switch(typeof elem) {
-        case "number":
-            return (elem * -1);
-            break;
-        case "boolean":
-            return !elem;
-            break;
-        case "string":
-            return "Nombre: " + elem;
-            break;
-    }
-});
-
-info.innerHTML = myArrayResult;
 
 
 
--- Práctica 2/main.js
+++ Práctica 2/main.js
@@ -2,7 +2,7 @@
 // EJERCICIO 1
 // ----------------------------------------
 class persona {
-    constructor(nombre,edad,genero) {  
+    constructor(nombre, edad, genero) {
         this.nombre = nombre;
         this.edad = edad;
         this.genero = genero;
@@ -10,8 +10,8 @@ class persona {
 }
 
 function objDetalles(persona) {
-    document.getElementById("persona").innerText+= "Nombre: " + persona.nombre + " Edad: " + persona.edad + " Género: " + persona.genero + "\n"; 
-    
+    document.getElementById("persona").innerText += "Nombre: " + persona.nombre + " Edad: " + persona.edad + " Género: " + persona.genero + "\n";
+
 }
 const persona1 = new persona("Jaime", 18, "Hombre");
 objDetalles(persona1);
@@ -19,7 +19,7 @@ objDetalles(persona1);
 // ----------------------------------------
 
 class estudiante extends persona {
-    constructor(nombre, edad, genero,curso, grupo) {
+    constructor(nombre, edad, genero, curso, grupo) {
         super(nombre, edad, genero);
         this.curso = curso;
         this.grupo = grupo;
@@ -27,16 +27,16 @@ class estudiante extends persona {
 }
 
 function registrar(estudiante) {
-    document.getElementById("estudiante").innerText = "Estudiante " + estudiante.nombre + " registrado correctamente en el curso " + estudiante.curso + estudiante.grupo +"\n";
+    document.getElementById("estudiante").innerText = "Estudiante " + estudiante.nombre + " registrado correctamente en el curso " + estudiante.curso + estudiante.grupo + "\n";
 }
-const estudiante1 = new estudiante("Jaime", 19,"Hombre",2,"DAW");
+const estudiante1 = new estudiante("Jaime", 19, "Hombre", 2, "DAW");
 registrar(estudiante1);
 
 // ----------------------------------------
 
 class profesor extends persona {
-    constructor(nombre,edad,genero,asignatura,nivel) {
-        super(nombre,edad,genero);
+    constructor(nombre, edad, genero, asignatura, nivel) {
+        super(nombre, edad, genero);
         this.asignatura = asignatura;
         this.nivel = nivel;
     }
@@ -45,11 +45,28 @@ class profesor extends persona {
 function asignar(profesor) {
     document.getElementById("profesor").innerText = "El profesor " + profesor.nombre + " con la asignatura " + profesor.asignatura + " y nivel " + profesor.nivel + " ha sido asignado\n";
 }
-const profesor1 = new profesor("Jaime", 19,"Hombre","DAWC","alto");
+const profesor1 = new profesor("Jaime", 19, "Hombre", "DAWC", "alto");
 asignar(profesor1);
 
 
 // ----------------------------------------
 // EJERCICIO 2
 // ----------------------------------------
+
+
+
+// ----------------------------------------
+// EJERCICIO 
+// ----------------------------------------
+document.getElementById("calDias").onclick = () => {
+    let fecNac = new Date(document.getElementById("fecNac").value);
+    let resfecNac = document.getElementById("resfecNac");
+
+    let dateNow = new Date();
+
+    resfecNac.innerText = (dateNow - fecNac)/1000/60/60/24/365 + " años vivo";
     
+}
+
+
+
